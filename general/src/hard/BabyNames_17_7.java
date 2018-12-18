da4cb8b2b56a04b19502fc4ddf809ebe00db4ce8
Ahmed <ahmed.kulovicepfl@gmail.com>
2018-12-18 07:48:50 +0100

    Started the baby names exercise

---
+++ general/src/hard/BabyNames_17_7.java
@@ -0,0 +1,20 @@
+package hard;
+
+import java.util.List;
+import java.util.Map;
+
+public class BabyNames_17_7 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static Map<String, Integer> getBabyNames(Map<String, Integer> freqs, List<List<String>> pairs) {
+		
+	}
+
+}
